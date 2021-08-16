a1aad2f3cbf1581b131f663f2894bf0cf9bfc453
6895mahfuzgit <6895mahfuz@gmail.com>
2021-08-16 23:19:57 +0600

    added helper module and  get counters,hierarchy using cv2

--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ Created on Sun Aug 15 22:31:29 2021
 """
 
 import cv2
+import helper as hp
+import numpy as np
+
 
 path='1.jpg'
 wigthImg=700
@@ -13,13 +16,27 @@ heightImg=700
 
 img=cv2.imread(path)
 img=cv2.resize(img,(wigthImg,heightImg))
+
+imgCounters=img.copy()
+
 imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur=cv2.GaussianBlur(imgGray,(5,5),1)
 imgCanny=cv2.Canny(imgBlur,10,50)
 
-cv2.imshow("Original",img)
+counters,hierarchy=cv2.findContours(imgCanny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(imgCounters,counters,-1,(0,0,255),10)
+
+imageBlank=np.zeros_like(img)
+
+imageArray=([img,imgGray,imgBlur,imgCanny],
+            [imgCounters,imageBlank,imageBlank,imageBlank])
+imgStacked=hp.stackImages(imageArray,0.5)
+
+
+#cv2.imshow("Original",img)
 #cv2.imshow("Gray",imgGray)
 #cv2.imshow("Blur",imgBlur)
-cv2.imshow("Canny",imgCanny)
+#cv2.imshow("Canny",imgCanny)
+cv2.imshow("Stacked Images",imgStacked)
 cv2.waitKey(0)
 
